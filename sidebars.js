390832403f9c1a19c39af463b730be09db8f0640
Flavio Castelli <fcastelli@suse.com>
2022-05-30 18:26:47 +0200

    Small reorg of the sidebar
    Move all the topics about writing policies under the same menu entry.
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- sidebars.js
+++ sidebars.js
@@ -8,10 +8,10 @@ module.exports = {
       items: ['tasksDir/mutating-policies', 'tasksDir/psp-migration'],
       collapsed: 'true',
       link: 
-          {
-            type: 'doc', 
-            id: 'tasks',
-          },
+      {
+        type: 'doc', 
+        id: 'tasks',
+      },
     },
     'architecture',
     {
@@ -19,86 +19,88 @@ module.exports = {
       label: 'Writing Policies',
       link: {type: 'doc', id: 'writing-policies/index'},
       items:[
-       {
-        'Policy Specification':
+        {
+          'Policy Specification':
+          [
+            { type: 'doc', id: 'writing-policies/spec/intro-spec'},
+            { type: 'doc', id: 'writing-policies/spec/settings'},
+            { type: 'doc', id: 'writing-policies/spec/validating-policies'},
+            { type: 'doc', id: 'writing-policies/spec/mutating-policies'},
+            { type: 'doc', id: 'writing-policies/spec/context-aware-policies'},
+            {
+              type: 'category',
+              label: 'Host Capabilities',
+              link: {type: 'doc', id: 'writing-policies/spec/host-capabilities/intro-host-capabilities'},
+              items: [
+                'writing-policies/spec/host-capabilities/signature-verifier-policies',
+                'writing-policies/spec/host-capabilities/container-registry',
+                'writing-policies/spec/host-capabilities/net'
+              ]
+            }
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Supported Languages',
+          items:[
+            {
+              'Rust':
               [
-                { type: 'doc', id: 'writing-policies/spec/intro-spec'},
-                { type: 'doc', id: 'writing-policies/spec/settings'},
-                { type: 'doc', id: 'writing-policies/spec/validating-policies'},
-                { type: 'doc', id: 'writing-policies/spec/mutating-policies'},
-                { type: 'doc', id: 'writing-policies/spec/context-aware-policies'},
-                {
-                  type: 'category',
-                  label: 'Host Capabilities',
-                  link: {type: 'doc', id: 'writing-policies/spec/host-capabilities/intro-host-capabilities'},
-                  items: [
-                    'writing-policies/spec/host-capabilities/signature-verifier-policies',
-                    'writing-policies/spec/host-capabilities/container-registry',
-                    'writing-policies/spec/host-capabilities/net'
-                  ]
-                }
+                'writing-policies/rust/intro-rust',
+                'writing-policies/rust/create-policy',
+                'writing-policies/rust/define-policy-settings',
+                'writing-policies/rust/write-validation-logic',
+                'writing-policies/rust/mutation-policy',
+                'writing-policies/rust/logging',
+                'writing-policies/rust/build-and-distribute',
               ],
-      },
-    ],
-  },
-  {  
-      type: 'category',
-      label: 'Supported Languages',
-      items:[
-        {
-          'Rust':
-          [
-            'writing-policies/rust/intro-rust',
-            'writing-policies/rust/create-policy',
-            'writing-policies/rust/define-policy-settings',
-            'writing-policies/rust/write-validation-logic',
-            'writing-policies/rust/mutation-policy',
-            'writing-policies/rust/logging',
-            'writing-policies/rust/build-and-distribute',
-          ],
-        'Go':
-          [
-            'writing-policies/go/intro-go',
-            'writing-policies/go/scaffold',
-            'writing-policies/go/policy-settings',
-            'writing-policies/go/validation',
-            'writing-policies/go/e2e-tests',
-            'writing-policies/go/logging',
-            'writing-policies/go/automate',
-            'writing-policies/go/distribute'
-          ],
-        'Rego':
-        [
-          'writing-policies/rego/intro-rego',
-          'writing-policies/rego/builtin-support',
-        ],
-        
-        'Swift':['writing-policies/swift',],
-        'TypeScript':['writing-policies/typescript',],
-        },
-     ],
-    },
-    {  
-      type: 'category',
-      label: 'Supported Frameworks',
-      items:[
-        {
-          'Open Policy Agent':
-          [
-            'writing-policies/rego/open-policy-agent/intro',
-            'writing-policies/rego/open-policy-agent/create-policy',
-            'writing-policies/rego/open-policy-agent/build-and-run',
-            'writing-policies/rego/open-policy-agent/distribute',
-          ],
-        'Gatekeeper':
-          [
-            'writing-policies/rego/gatekeeper/intro',
-            'writing-policies/rego/gatekeeper/create-policy',
-            'writing-policies/rego/gatekeeper/build-and-run',
-            'writing-policies/rego/gatekeeper/distribute',
+              'Go':
+              [
+                'writing-policies/go/intro-go',
+                'writing-policies/go/scaffold',
+                'writing-policies/go/policy-settings',
+                'writing-policies/go/validation',
+                'writing-policies/go/e2e-tests',
+                'writing-policies/go/logging',
+                'writing-policies/go/automate',
+                'writing-policies/go/distribute'
+              ],
+              'Rego':
+              [
+                'writing-policies/rego/intro-rego',
+                'writing-policies/rego/builtin-support',
+              ],
+
+              'Swift':['writing-policies/swift',],
+              'TypeScript':['writing-policies/typescript',],
+            },
           ],
         },
-     ],
+
+        {  
+          type: 'category',
+          label: 'Supported Frameworks',
+          items:[
+            {
+              'Open Policy Agent':
+              [
+                'writing-policies/rego/open-policy-agent/intro',
+                'writing-policies/rego/open-policy-agent/create-policy',
+                'writing-policies/rego/open-policy-agent/build-and-run',
+                'writing-policies/rego/open-policy-agent/distribute',
+              ],
+              'Gatekeeper':
+              [
+                'writing-policies/rego/gatekeeper/intro',
+                'writing-policies/rego/gatekeeper/create-policy',
+                'writing-policies/rego/gatekeeper/build-and-run',
+                'writing-policies/rego/gatekeeper/distribute',
+              ],
+            },
+          ],
+        },
+
+      ],
     },
     {
       type: 'category',
@@ -106,26 +108,28 @@ module.exports = {
       link: {type: 'doc', id: 'distributing-policies',},
       items:['distributing-policies/custom-certificate-authorities', 'distributing-policies/oci-registries-support', 'distributing-policies/secure-supply-chain',],
     },
+
+
     {
       type: 'category',
       label: 'Testing Policies',
       link: {type: 'doc', id: 'testing-policies/intro'},
       items:[
-       {
-        'User Personas':
-              [
-                'testing-policies/policy-authors',
-                'testing-policies/cluster-operators',
-              ],
-      },
-     ],
+        {
+          'User Personas':
+          [
+            'testing-policies/policy-authors',
+            'testing-policies/cluster-operators',
+          ],
+        },
+      ],
     },
     {
-        type: 'category',
-        label: 'Operator Manual',
-        link: {type: 'doc', id: 'operator-manual/intro'},
-        items:[
-         {
+      type: 'category',
+      label: 'Operator Manual',
+      link: {type: 'doc', id: 'operator-manual/intro'},
+      items:[
+        {
           'Configuring Policy Servers': ['operator-manual/policy-servers/custom-cas',],
           'Quickstart Guides':
           [
@@ -141,6 +145,6 @@ module.exports = {
           'Monitor Mode': ['operator-manual/monitor-mode/intro',],
         },
       ],
-     },
+    },
   ],
 };
