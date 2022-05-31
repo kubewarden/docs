1741468d569988fa1dc6c99c71f48206d85b4490
Flavio Castelli <fcastelli@suse.com>
2022-05-31 08:54:36 +0200

    Enable theme switch button
    Allow users to switch between light and dark mode.
    Both themes look great on our website :)
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
       // Hides the switch in the navbar
       // Useful if you want to support a single color mode
-      disableSwitch: true,
+      disableSwitch: false,
     },
     navbar: {
       title: "",
